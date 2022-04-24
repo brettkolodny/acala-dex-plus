a16f75cecee793f5bddeee64cf888eb1ab434795
brettkolodny <brettkolodny@gmail.com>
2022-04-24 19:00:00 -0400

    feat: polkadot-js extension support

--- src/lib/ExtensionSelect/AccountList.svelte
+++ src/lib/ExtensionSelect/AccountList.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import TalismanIcon from "../../assets/icons/talisman-icon.svg";
+  import PolkadotjsIcon from "../../assets/icons/polkadot-js-icon.svg";
 
   export let accounts: Array<{ name?: string; address: string }>;
   export let createBodhiSigner: (address: string) => void;
-  // export let isTalisman = true;
+  export let isTalisman = true;
 
   const formatAddress = (address: string): string => {
     return `${address.slice(0, 8)}...${address.slice(address.length - 8)}`;
@@ -13,9 +14,13 @@
 {#each accounts as account}
   <div
     on:click={() => createBodhiSigner(account.address)}
-    class="flex flex-row justify-center items-center gap-2 w-64 h-16 bg-white dark:bg-base-100 rounded-lg cursor-pointer"
+    class="flex flex-row justify-start items-center gap-2 w-full min-w-[256px] h-16 pl-4 bg-white dark:bg-base-100 rounded-lg cursor-pointer"
   >
-    <img src={TalismanIcon} alt="talisman account" />
+    {#if isTalisman}
+      <img class="w-10" src={TalismanIcon} alt="talisman account" />
+    {:else}
+      <img class="w-10" src={PolkadotjsIcon} alt="polkadot-js account" />
+    {/if}
     <div class="flex flex-col justify-center items-start">
       <div class="text-xl font-semibold">{account.name}</div>
       <div class="text-sm text-base-600">
